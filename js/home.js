197da9c241bdd7671206d74155f89fc8c1a45a51
“Diêgo <diegovbarros@gmail.com>
2018-12-18 12:21:56 -0200

    [014] - Desafio Básico

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 const $button   = window.document.querySelector('.-second');
+let initialValue = 10;
 $button.addEventListener("click",(event) => {
     const $cart     = window.document.querySelector('.navigation a:last-child ');
-    $cart.textContent = "Burpe";
+    $cart.textContent = `Carrinho (${++initialValue})`;
 })
--- js/product-description.js
+++ js/product-description.js
@@ -1,4 +1,5 @@
 const $heart = window.document.querySelector('.-heart');
-$heart.addEventListener("click",(event) => {
-    console.log(event.target)
+$heart.addEventListener('click',(event) => {
+    const $img = $heart.querySelector('img');
+    $img.classList.toggle('-active')
 })
