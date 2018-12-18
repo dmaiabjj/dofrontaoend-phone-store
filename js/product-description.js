2806a95e02f974ed65938bf8347816183b411942
“Diêgo <diegovbarros@gmail.com>
2018-12-18 13:15:06 -0200

    [014] - Desafio Avançado

--- js/product-description.js
+++ js/product-description.js
@@ -1,14 +1,22 @@
 const $heart = window.document.querySelector('.-heart');
-$heart.addEventListener('click',(event) => {
+$heart.addEventListener('click',() => {
     const $img = $heart.querySelector('img');
     $img.classList.toggle('-active')
 })
 
 const $stars = window.document.querySelectorAll('.-star > img');
-$stars.forEach($star => {
-    $star.addEventListener('click',(event) => {
-        var img1 = "img/star-filled.png",img2 = "img/star-not-filled.png";
-        console.log($star.src)
-        $star.src =  ($star.src.includes(img1))? img2 : img1;
+$stars.forEach((star,index,stars)=> {
+    star.addEventListener('click',() => {
+        fillStars(stars,index)
     })
 });
+
+function fillStars(stars,indexClicked) {
+    var img1 = "img/star-filled.png",img2 = "img/star-not-filled.png";
+    stars.forEach((star,index)=> {
+        if(index <= indexClicked)
+            star.src = img1;
+        else
+            star.src = img2;
+    });
+}
