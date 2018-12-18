880efb7194ab93254b678cd08faa82c85f753727
“Diêgo <diegovbarros@gmail.com>
2018-12-18 10:08:39 -0200

    [013] - Correção Desafio Básico

--- js/button-store.js
+++
@@ -1,6 +0,0 @@
-const $button   = window.document.querySelector('.-second');
-const $cart     = window.document.querySelector('.navigation a:last-child ');
-
-$button.addEventListener("click",(event) => {
-    $cart.innerHTML = "Burpe";
-})
