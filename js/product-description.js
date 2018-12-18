c86c90dca3b30759d8df1295cb8f4f1e7350a2dc
“Diêgo <diegovbarros@gmail.com>
2018-12-18 13:05:51 -0200

    [014] - Desafio Intermediário

--- js/product-description.js
+++ js/product-description.js
@@ -3,3 +3,12 @@ $heart.addEventListener('click',(event) => {
     const $img = $heart.querySelector('img');
     $img.classList.toggle('-active')
 })
+
+const $stars = window.document.querySelectorAll('.-star > img');
+$stars.forEach($star => {
+    $star.addEventListener('click',(event) => {
+        var img1 = "img/star-filled.png",img2 = "img/star-not-filled.png";
+        console.log($star.src)
+        $star.src =  ($star.src.includes(img1))? img2 : img1;
+    })
+});
