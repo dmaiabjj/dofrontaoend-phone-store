7eee0906a4ecedd97a3c53b41dc66e6903c6c7f6
“Diêgo <diegovbarros@gmail.com>
2018-12-19 13:02:16 -0200

    [014] - Correção do Desafio Intermediário

--- js/product-description.js
+++ js/product-description.js
@@ -1,21 +1,11 @@
 const $heart = window.document.querySelector('.-heart');
-$heart.addEventListener('click',() => {
-    $heart.classList.toggle('-active')
-})
+$heart.addEventListener('click',handleClick)
 
-const $stars = window.document.querySelectorAll('.-star > img');
-$stars.forEach((star,index,stars)=> {
-    star.addEventListener('click',() => {
-        fillStars(stars,index)
-    })
+const $stars = window.document.querySelectorAll('.item.-star .star');
+$stars.forEach(($star)=> {
+    $star.addEventListener('click',handleClick)
 });
 
-function fillStars(stars,indexClicked) {
-    var img1 = "img/star-filled.png",img2 = "img/star-not-filled.png";
-    stars.forEach((star,index)=> {
-        if(index <= indexClicked)
-            star.src = img1;
-        else
-            star.src = img2;
-    });
+function handleClick(){
+    this.classList.toggle('-active')
 }
