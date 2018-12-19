99e3caf7cfc95b008f938ce922cf82d13389de5f
“Diêgo <diegovbarros@gmail.com>
2018-12-19 13:42:47 -0200

    [015] - Desafio Único

--- js/product-description.js
+++ js/product-description.js
@@ -2,10 +2,25 @@ const $heart = window.document.querySelector('.-heart');
 $heart.addEventListener('click',handleClick)
 
 const $stars = window.document.querySelectorAll('.item.-star .star');
-$stars.forEach(($star)=> {
-    $star.addEventListener('click',handleClick)
+$stars.forEach(($star,key,$stars)=> {
+     $star.addEventListener('click',() =>{
+        fillStars($star,key,$stars)
+    })
 });
 
 function handleClick(){
     this.classList.toggle('-active')
+}
+
+function fillStars($star,key,$stars) {
+    $star.classList.toggle('-active')
+    const active = $star.classList.contains('-active');
+    $stars.forEach((star,index)=> {
+        if(index <= key)
+        {
+            if(active)
+                star.classList.add('-active')
+        }else
+            star.classList.remove('-active')
+    });
 }
